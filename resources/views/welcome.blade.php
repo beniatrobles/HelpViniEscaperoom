6b4209cf9f5f94785739b761730cd87a1dbad2b8
ik_2dw3a <ik_2dw3a@H015110.NET>
2024-12-05 09:39:39 +0100

    Borrar gitignore

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vue in Laravel</title>
+    @vite('resources/js/app.js') <!-- Esto carga el archivo JavaScript que contiene Vue -->
+</head>
+<body>
+    <div id="app"></div> <!-- Este es el contenedor donde Vue montará la aplicación -->
+</body>
+</html>
