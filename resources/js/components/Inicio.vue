218ea1b1c2d0b7feb7d2471c7364dfe35c6ef6ec
ik_2dw3a <ik_2dw3a@H015110.NET>
2024-12-10 12:57:55 +0100

    pantalla de inicio

--- resources/js/components/Inicio.vue
+++ resources/js/components/Inicio.vue
@@ -2,16 +2,15 @@
 </script>
 
 <template>
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Help Vini - Inicio</title>
-</head>
 
-<div class="mesa h-screen">
-<body>
-    
-</body>
+<div class="mesa h-screen relative overflow-hidden">
+  <router-link to="/" class="absolute w-[250px] rotate-[-10deg] top-[200px] right-[45%] hover:scale-[1.05] hover:z-10 duration-200 hover:rotate-[-8deg]"> <img :src="'/img/revista.jpg'" > </router-link>
+  <router-link to="/" class="absolute w-[350px] rotate-[-80deg] top-0 right-[25%]  hover:scale-[1.05] hover:z-10 duration-200 hover:rotate-[-83deg]"> <img :src="'/img/ipad.png'" > </router-link>
+  <router-link to="/" class="absolute w-[100px] rotate-[-15deg] bottom-[100px] right-[-1%] hover:scale-[1.05] hover:z-10 duration-200 hover:rotate-[-10deg]"> <img :src="'/img/polaroid.jpg'" > </router-link>
+  <router-link to="/" class="absolute w-[100px] rotate-[5deg] top-[100px] left-[5%] hover:scale-[1.05] hover:z-10 duration-200 hover:rotate-0"> <div class="h-[100px] w-[100px] bg-yellow-600 text-center text-black "><p>_________</p>_______<p>___________</p></div> </router-link>
+ <img :src="'/img/boli.jpg'" class="absolute w-[100px] rotate-[-20deg] top-[100px] left-[10%] z-10">
+ <img :src="'/img/clips.png'" class="absolute w-[100px] rotate-[-20deg] top-[200px] left-[5%] z-10">
+  
 </div>
 </template>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Vue in Laravel</title>
+    <title>Help Vini</title>
     @vite('resources/js/app.js') <!-- Esto carga el archivo JavaScript que contiene Vue -->
 </head>
 <body>
