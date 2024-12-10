bb9ee75e99de91b592d182fa83ed2c451c2f7687
iimanol7 <149791709+iimanol7@users.noreply.github.com>
2024-12-10 21:24:45 +0100

    mas objetos añadidos a la mesa

--- resources/js/components/Inicio.vue
+++ resources/js/components/Inicio.vue
@@ -4,14 +4,21 @@
 <template>
 
 <div class="mesa h-screen relative overflow-hidden">
-  <router-link to="/" class="absolute w-[250px] rotate-[-10deg] top-[200px] right-[45%] hover:scale-[1.05] hover:z-10 duration-200 hover:rotate-[-8deg]"> <img :src="'/img/revista.jpg'" > </router-link>
-  <router-link to="/" class="absolute w-[350px] rotate-[-80deg] top-0 right-[25%]  hover:scale-[1.05] hover:z-10 duration-200 hover:rotate-[-83deg]"> <img :src="'/img/ipad.png'" > </router-link>
-  <router-link to="/" class="absolute w-[100px] rotate-[-15deg] bottom-[100px] right-[-1%] hover:scale-[1.05] hover:z-10 duration-200 hover:rotate-[-10deg]"> <img :src="'/img/polaroid.jpg'" > </router-link>
-  <router-link to="/" class="absolute w-[110px] rotate-[5deg] top-[100px] left-[5%] hover:scale-[1.05] hover:z-10 duration-200 hover:rotate-0"> <!-- <div class="h-[100px] w-[100px] bg-yellow-600 text-center text-black "><p>_________</p>_______<p>___________</p></div>--> <img :src="'/img/postit.png'" alt="" class="brightness-75"></router-link> 
+  <router-link to="/" class="absolute w-[350px] rotate-[-80deg] top-0 right-[25%]  z-10 hover:scale-[1.05] hover:z-20 duration-200 hover:rotate-[-83deg]"> <img :src="'/img/ipad.png'" > </router-link>
+  <router-link to="/" class="absolute w-[250px] rotate-[-10deg] top-[200px] right-[45%] hover:scale-[1.05] hover:z-20 duration-200 hover:rotate-[-8deg]"> <img :src="'/img/revista.jpg'" > </router-link>
+  <router-link to="/" class="absolute w-[100px] rotate-[-10deg] bottom-[100px] right-[-1%] hover:scale-[1.05] hover:z-10 duration-200 hover:rotate-[-5deg]"> <img :src="'/img/polaroid.jpg'" > </router-link>
+  <router-link to="/" class="absolute w-[120px] rotate-[-10deg] top-[100px] left-[5%] hover:scale-[1.05] hover:z-10 duration-200 hover:rotate-0"> <!-- <div class="h-[100px] w-[100px] bg-yellow-600 text-center text-black "><p>_________</p>_______<p>___________</p></div>--> <img :src="'/img/postit.png'" alt="" class="brightness-75"></router-link> 
   
  <img :src="'/img/boli.jpg'" class="absolute w-[100px] rotate-[-20deg] top-[100px] left-[10%] z-10">
  <img :src="'/img/clips.png'" class="absolute w-[100px] rotate-[-20deg] top-[200px] left-[5%] z-10">
  <img :src="'/img/platano.png'" class="absolute w-[250px] bottom-[6%] left-[-7%] z-10 brightness-75">
+ 
+ <div class=" absolute top-[20%] right-[7%] flex rotate-[-10deg] cursor-pointer transition duration-200 hover:scale-[1.05] hover:rotate-[-5deg]">
+  <img :src="'/img/airpods.png'" class=" w-[80px] ">
+  <img :src="'/img/airpods.png'" class=" w-[80px] rotate-[-20deg] scale-x-[-1]">
+</div>
+<router-link to="/" class="absolute right-[25%] bottom-[0] w-[200px] rotate-[82deg] hover:scale-[1.05] hover:z-20 duration-200 hover:rotate-[80deg]"> <img :src="'/img/cuaderno.png'" > </router-link>
+
   
 </div>
 </template>
