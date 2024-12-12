29d17b37c6d3c25fea65d8604109b375c774fc5d
iimanol7 <149791709+iimanol7@users.noreply.github.com>
2024-12-12 21:37:35 +0100

    registro funcional

--- resources/js/components/Registro.vue
+++ resources/js/components/Registro.vue
@@ -1,17 +1,23 @@
 <template>
-    <div class="flex justify-center items-center h-screen">
-        <form @submit.prevent="enviarFormulario" class="border-[1px] p-[10px]">
-            <h1 class="text-center">Registrate</h1>
+    <div class="flex justify-center items-center flex-col h-screen">
+        <div class=" w-[300px] mb-2">
+            <router-link to="/" class="hover:text-[#0ED800] flex items-center font-bold max-w-max"><span
+                    class="text-2xl">←</span>
+                Volver</router-link>
+        </div>
+        <form @submit.prevent="enviarFormulario"
+            class="bg-zinc-1000 flex flex-col justify-center items-center border-[1px]  max-w-[300px] w-[100%] h-[350px] rounded">
+            <h1 class=" mb-6 text-xl">Registrate</h1>
             <input type="text" v-model="nombre" name="nombre" placeholder="Nombre de Usuario" />
             <br />
-            <input type="email" v-model="email" name="email" placeholder="Email"/>
+            <input type="email" v-model="email" name="email" placeholder="Email" />
+            <br />
+            <input type="password" v-model="password" name="password" placeholder="Contraseña" />
             <br />
 
-            <label for="password">Password</label>
-            <input type="password" v-model="password" name="password" />
-            <br />
-
-            <button type="submit">Registrarme</button>
+            <button type="submit" class=" relative p-[5px] bg-white text-black w-[50%] rounded flex hover:text-white font-bold">
+                <p class="z-[10] text-center w-[100%]">Registrarme</p>
+            </button>
         </form>
     </div>
 </template>
@@ -29,14 +35,14 @@ const password = ref("");
 // Method to handle form submission
 const enviarFormulario = () => {
     axios
-        .post("/registro", {
+        .post("/register", {
             nombre: nombre.value,
             email: email.value,
-            password: password.value,
+            pass: password.value,
         })
         .then((res) => {
             // Redirect to login upon success
-            router.get("/login");
+            router.get("/");
         })
         .catch((err) => {
             // Show an alert on error
@@ -44,3 +50,39 @@ const enviarFormulario = () => {
         });
 };
 </script>
+<style scoped>
+input {
+    color: black;
+    height: 40px;
+    width: 250px;
+    padding: 5px 10px;
+    border-radius: 5px;
+    box-shadow: 4px 4px 0 gray;
+}
+
+input:focus {
+    outline: 1px solid #0ED800;
+    box-shadow: 4px 4px 0 #0ED800;
+}
+
+form {
+    box-shadow: 5px 5px 0 white;
+}
+
+button::after {
+    content: '';
+    background-color: #0ED800;
+    position: absolute;
+    left: 0;
+    right: 0;
+    margin: auto;
+    top: 0;
+    width: 0%;
+    height: 100%;
+    border-radius: 5px;
+    transition: .3s;
+}
+button:hover::after{
+    width: 100%;
+}
+</style>
--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/admin/prueba', [AdminController::class, 'index'])->name('admin.prueba');
-
+Route::post('/register', [AuthController::class, 'registro']);
 Route::get('/{any}', function () {
     return view('welcome'); // Vue se monta aquí
 })->where('any', '.*');
