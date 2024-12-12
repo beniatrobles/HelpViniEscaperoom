4cc82c44d5b5c1a400769bea037f67dc3f15e6b5
ik_2dw3a <ik_2dw3a@H015110.NET>
2024-12-12 13:17:03 +0100

    formulario de registro(sin terminar)

--- resources/js/components/Registro.vue
+++ resources/js/components/Registro.vue
@@ -1,24 +1,22 @@
 <template>
-    <div>
-      <form @submit.prevent="enviarFormulario" style="display: block">
-        <label for="nombre">Nombre</label>
-        <input type="text" v-model="nombre" name="nombre" />
-        <br />
-  
-        <label for="email">Email</label>
-        <input type="email" v-model="email" name="email" />
-        <br />
-  
-        <label for="password">Password</label>
-        <input type="password" v-model="password" name="password" />
-        <br />
-  
-        <button type="submit">Registrarme</button>
-      </form>
+    <div class="flex justify-center items-center h-screen">
+        <form @submit.prevent="enviarFormulario" class="border-[1px] p-[10px]">
+            <h1 class="text-center">Registrate</h1>
+            <input type="text" v-model="nombre" name="nombre" placeholder="Nombre de Usuario" />
+            <br />
+            <input type="email" v-model="email" name="email" placeholder="Email"/>
+            <br />
+
+            <label for="password">Password</label>
+            <input type="password" v-model="password" name="password" />
+            <br />
+
+            <button type="submit">Registrarme</button>
+        </form>
     </div>
-  </template>
-  
-  <script setup>
+</template>
+
+<script setup>
 import { ref } from "vue";
 import axios from "axios";
 import { router } from "@inertiajs/vue3";
@@ -30,19 +28,19 @@ const password = ref("");
 
 // Method to handle form submission
 const enviarFormulario = () => {
-  axios
-    .post("/registro", {
-      nombre: nombre.value,
-      email: email.value,
-      password: password.value,
-    })
-    .then((res) => {
-      // Redirect to login upon success
-      router.get("/login");
-    })
-    .catch((err) => {
-      // Show an alert on error
-      alert("Error al registrar al usuario");
-    });
+    axios
+        .post("/registro", {
+            nombre: nombre.value,
+            email: email.value,
+            password: password.value,
+        })
+        .then((res) => {
+            // Redirect to login upon success
+            router.get("/login");
+        })
+        .catch((err) => {
+            // Show an alert on error
+            alert("Error al registrar al usuario");
+        });
 };
 </script>
