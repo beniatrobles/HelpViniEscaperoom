6d7c45cb4df24ed2c1806b2914587d6e21a69c6c
iimanol7 <149791709+iimanol7@users.noreply.github.com>
2024-12-14 18:05:04 +0100

    rutas en un archivo a parte

--- resources/js/components/Mesa/Airpods.vue
+++ resources/js/components/Mesa/Airpods.vue
@@ -4,11 +4,18 @@
 <template>
 
     <div class="flex flex-col justify-center items-center min-h-screen bg-opacity-50 bg-black">
-        <router-link to="/inicio"><h1 class="absolute top-5 right-5 text-[30px]">X</h1></router-link>
-        
-        <div class="h-[250px] absolute flex rotate-[-10deg]">
-            <img :src="'/img/airpods.png'" class="">
-            <img :src="'/img/airpods.png'" class="rotate-[-20deg] scale-x-[-1]">
+        <router-link to="/inicio">
+            <h1 class="absolute top-5 right-5 text-[30px]">X</h1>
+        </router-link>
+
+        <div class="h-[250px] flex ">
+            <img :src="'/img/airpods.png'" class="w-[250px]">
+            <img :src="'/img/airpods.png'" class="w-[250px] rotate-[-20deg] scale-x-[-1]">
+        </div>
+        <div>
+                <audio controls>
+                    <source :src="'/audio/funk.mp3'" type="audio/mp3">
+                </audio>
         </div>
 
         <router-link to=""> </router-link>
