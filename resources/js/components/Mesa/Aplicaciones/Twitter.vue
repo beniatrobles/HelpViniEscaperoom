49b2e88153ad268d030e57dc615ffea0d3b06bf1
AitorLo12 <aitorlpgn@gmail.com>
2025-01-08 11:54:00 +0100

    twitter perfil

--- resources/js/components/Mesa/Aplicaciones/Twitter.vue
+++ resources/js/components/Mesa/Aplicaciones/Twitter.vue
@@ -2,7 +2,7 @@
     <div class="flex justify-center items-center min-h-screen bg-white bg-opacity-10">
         <router-link to="/inicio"><h1 class="absolute top-5 right-5 text-[30px]">X</h1></router-link>
         <div class="w-[1000px] h-[600px] rounded-3xl border-r-[40px] border-l-[30px] border-t-[15px] border-b-[15px] border-black shadow-md relative">
-            <div class="flex bg-gray-900 h-[100%] items-center justify-center px-20">
+            <div class="flex bg-gray-900 h-[100%] items-center justify-center pl-20 pr-10">
                 <div class="h-[100%] w-[25%]">
                     <div class="flex items-center justify-start my-3">
                         <router-link to="/inicio/tablet/twitter">
@@ -49,7 +49,7 @@
                 </div>
 
                 <div class="h-[100%] w-[75%]">
-                    <router-view></router-view>
+                    <router-view class="overflow-auto"></router-view>
                 </div>
             <router-link to="/inicio/tablet" class="absolute top-[280px] right-[-32px] w-[25px] h-[25px] border-white border-[1px] rounded-3xl bg-black cursor-pointer"></router-link>
             </div>
