e9ff302f39ad0e9c255c13ba39dc1840fbe530f5
imanol <imanolgullon@gmail.com>
2025-02-26 17:29:38 +0100

    pinterest

--- resources/js/components/Mesa/Aplicaciones/Pinterest.vue
+++ resources/js/components/Mesa/Aplicaciones/Pinterest.vue
@@ -3,9 +3,17 @@
         <router-link to="/inicio"><h1 class="absolute top-5 right-5 text-[30px]">X</h1></router-link>
         <div class="bg-zinc-800 w-[1000px] h-[600px] rounded-3xl border-r-[40px] border-l-[30px] border-t-[15px] border-b-[15px] border-black shadow-md relative flex ">
             
-            <div class="h-full w-[20%] border-r flex flex-col p-2">
-                <router-link to="/inicio/tablet/pinterest" class="hover:bg-zinc-700 rounded p-2">Inicio</router-link>
-                <router-link to="/inicio/tablet/pinterest/perfil" class="hover:bg-zinc-700 rounded p-2">Tableros</router-link>
+            <div class="h-full w-16 border-r flex flex-col p-2 gap-1 bg-zinc-900">
+                <router-link to="/inicio/tablet/pinterest" class="hover:bg-zinc-700 rounded-full overflow-hidden max-w-max mx-auto" @click="activarLink('inicio')"><img :src="'/storage/img/apps/pinterest.png'" class="w-8"></router-link>
+
+                <router-link to="/inicio/tablet/pinterest" :class="linkSeleccionado == 'inicio' ? 'hidden' : ''" class="hover:bg-zinc-700 rounded p-3 mt-7" @click="activarLink('inicio')"><img :src="'/storage/img/apps/iconos/icon-inicio-tw.svg'"></router-link>
+                <router-link to="/inicio/tablet/pinterest" :class="linkSeleccionado != 'inicio' ? 'hidden' : ''" class="hover:bg-zinc-700 rounded p-3 mt-7"><img :src="'/storage/img/apps/iconos/icon-inicio-F.png'" class=""></router-link>
+
+                <router-link to="/inicio/tablet/pinterest/notificaciones" :class="{'hidden' : linkSeleccionado == 'notif'}" class="hover:bg-zinc-700 rounded p-3" @click="activarLink('notif')"><img :src="'/storage/img/apps/iconos/icon-notificaciones.svg'"></router-link>
+                <router-link to="/inicio/tablet/pinterest/notificaciones" :class="{'hidden' : linkSeleccionado != 'notif'}" class="hover:bg-zinc-700 rounded p-3"><img :src="'/storage/img/apps/iconos/icon-notificacionesF.svg'"></router-link>
+
+                <router-link to="/inicio/tablet/pinterest/perfil" :class="{'hidden' : linkSeleccionado == 'perfil'}" class="hover:bg-zinc-700 rounded p-3" @click="activarLink('perfil')"><img :src="'/storage/img/apps/iconos/icon-perfil-tw.png'"></router-link>
+                <router-link to="/inicio/tablet/pinterest/perfil" :class="{'hidden' : linkSeleccionado != 'perfil'}" class="hover:bg-zinc-700 rounded p-3"><img :src="'/storage/img/apps/iconos/icon-perfil-twF.png'"></router-link>
             </div>
 
             <router-view></router-view>
@@ -17,4 +25,10 @@
 
 
 <script setup>
+import { ref } from 'vue';
+
+const linkSeleccionado = ref('inicio')
+
+const activarLink = (link) => linkSeleccionado.value = link;  
+
 </script>
--- resources/js/components/Mesa/Aplicaciones/Pinterest/PinterestPerfil.vue
+++ resources/js/components/Mesa/Aplicaciones/Pinterest/PinterestPerfil.vue
@@ -1,6 +1,42 @@
 <template>
-    <h1>PERFIL</h1>
+    <div class="w-full overflow-auto py-8 px-20">
+        <div class="flex justify-between">
+            <p class="text-xl font-extrabold">Tus ideas guardadas</p>
+            <div class="w-8 h-8 bg-gray-300 rounded-full"></div>
+        </div>
+        <div class="mt-6 flex gap-5">
+            <router-link :to="{name : 'pines'}" :class="{'active' : linkSeleccionado ==1}" class="links font-medium relative" @click="activarLink(1)">Pines</router-link>
+            <router-link :to="{name : 'tableros'}" :class="{'active' : linkSeleccionado ==2}" class="links font-medium relative" @click="activarLink(2)">Tableros</router-link>
+        </div>
+
+        <router-view class="mt-5"></router-view>
+    </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
+
+const linkSeleccionado = ref(2)
+
+const activarLink = (n) => linkSeleccionado.value = n
+
 </script>
+
+<style scoped>
+    .links::after{
+        content: '';
+        width: 0%;
+        height: 3px;
+        background-color: white;
+        position: absolute;
+        bottom: -3px;
+        left: 0;
+        transition: .2s;
+    }
+    .links.active::after{
+        width: 100%;
+    }
+    .links:hover::after{
+        width: 100%;
+    }
+</style>
