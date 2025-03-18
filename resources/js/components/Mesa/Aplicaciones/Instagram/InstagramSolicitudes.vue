0a8e257cdaa2774f98da77b416e6867fb0bcfd7a
imanol <imanolgullon@gmail.com>
2025-03-18 17:44:48 +0100

    apartado de solicitudes de mensajes en ig hecho

---
+++ resources/js/components/Mesa/Aplicaciones/Instagram/InstagramSolicitudes.vue
@@ -0,0 +1,85 @@
+<template>
+    <div class="flex gap-2 h-[100%] mt-3">
+        <div class="w-[35%] border-r border-zinc-800 overflow-auto">
+            <router-link to="/inicio/tablet/instagram/mensajes/solicitudes/solicitud1"
+                :class="{ 'bg-[#2f2f2f]': linkActivo === 1 }"
+                class="flex gap-3 p-1 rounded cursor-pointer hover:bg-zinc-800" @click="activarLink(1)">
+                <img :src="'https://ih1.redbubble.net/image.420452072.3078/st,small,507x507-pad,600x600,f8f8f8.u5.jpg'" class="w-[50px] h-[50px] rounded-full">
+                <div>
+                    <div class="flex items-center gap-2">
+                        <p class="font-bold flex items-center gap-1">aitorlpgn</p>
+                    </div>
+                    <p class="whitespace-nowrap text-sm text-gray-300">recibido</p>
+                </div>
+
+            </router-link>
+            <router-link to="/inicio/tablet/instagram/mensajes/solicitudes/solicitud2"
+                :class="{ 'bg-[#2f2f2f]': linkActivo === 2 }"
+                class="flex gap-3 p-1 rounded cursor-pointer hover:bg-zinc-800" @click="activarLink(2)">
+                <img :src="'https://ih1.redbubble.net/image.420452072.3078/st,small,507x507-pad,600x600,f8f8f8.u5.jpg'" class="w-[50px] h-[50px] rounded-full">
+                <div>
+                    <div class="flex items-center gap-2">
+                        <p class="font-bold flex items-center gap-1">usuario</p>
+                    </div>
+                    <p class="whitespace-nowrap text-sm text-gray-300">recibido</p>
+                </div>
+
+            </router-link>
+            <router-link to="/inicio/tablet/instagram/mensajes/solicitudes/solicitud3"
+                :class="{ 'bg-[#2f2f2f]': linkActivo === 3 }"
+                class="flex gap-3 p-1 rounded cursor-pointer hover:bg-zinc-800" @click="activarLink(3)">
+                <img :src="'https://ih1.redbubble.net/image.420452072.3078/st,small,507x507-pad,600x600,f8f8f8.u5.jpg'" class="w-[50px] h-[50px] rounded-full">
+                <div>
+                    <div class="flex items-center gap-2">
+                        <p class="font-bold flex items-center gap-1">usuario</p>
+                    </div>
+                    <p class="whitespace-nowrap text-sm text-gray-300">recibido</p>
+                </div>
+
+            </router-link>
+            <router-link to="/inicio/tablet/instagram/mensajes/solicitudes/solicitud4"
+                :class="{ 'bg-[#2f2f2f]': linkActivo === 4 }"
+                class="flex gap-3 p-1 rounded cursor-pointer hover:bg-zinc-800" @click="activarLink(4)">
+                <img :src="'https://ih1.redbubble.net/image.420452072.3078/st,small,507x507-pad,600x600,f8f8f8.u5.jpg'" class="w-[50px] h-[50px] rounded-full">
+                <div>
+                    <div class="flex items-center gap-2">
+                        <p class="font-bold flex items-center gap-1">usuario</p>
+                    </div>
+                    <p class="whitespace-nowrap text-sm text-gray-300">recibido</p>
+                </div>
+
+            </router-link>
+            <router-link to="/inicio/tablet/instagram/mensajes/solicitudes/solicitud5"
+                :class="{ 'bg-[#2f2f2f]': linkActivo === 5 }"
+                class="flex gap-3 p-1 rounded cursor-pointer hover:bg-zinc-800" @click="activarLink(5)">
+                <img :src="'https://ih1.redbubble.net/image.420452072.3078/st,small,507x507-pad,600x600,f8f8f8.u5.jpg'" class="w-[50px] h-[50px] rounded-full">
+                <div>
+                    <div class="flex items-center gap-2">
+                        <p class="font-bold flex items-center gap-1">usuario</p>
+                    </div>
+                    <p class="whitespace-nowrap text-sm text-gray-300">recibido</p>
+                </div>
+
+            </router-link>
+            
+        </div>
+        <div class="w-[65%] relative">
+            <div
+                class="flex flex-col justify-center items-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                <img :src="'/storage/img/icon-direct.png'">
+                <p>Tus mensajes</p>
+            </div>
+            <router-view class="absolute"></router-view>
+        </div>
+    </div>
+</template>
+
+
+<script setup>
+import { ref } from 'vue';
+const linkActivo = ref('')
+
+const activarLink = link => linkActivo.value = link
+
+
+</script>
---
+++ resources/js/components/Mesa/Aplicaciones/Instagram/mensajes/solicitudes/Solicitud5.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="flex flex-col w-[100%] h-[100%] bg-zinc-900 border-r border-zinc-800 pr-2">
+        <div class="flex h-full pb-4">
+            <div class="flex flex-col gap-1 mt-auto w-full">
+                <p class="p-4 bg-zinc-700 max-w-[80%] rounded-[20px] w-max">P*to mono🦍🍌</p>
+                <p class="p-4 bg-zinc-700 max-w-[80%] rounded-[20px] w-max">Que eres un payaso🖕🖕</p>
+                <p class="p-4 bg-zinc-700 max-w-[80%] rounded-[20px] w-max">Y feo</p>
+                <img src="https://pbs.twimg.com/media/FqEqV1YXwAIsNR9.jpg:large" class="w-[150px] rounded-lg border border-zinc-800">
+            </div>
+        </div>
+        <div class="mt-auto flex gap-2">
+            <input type="text" placeholder="Mensaje"
+                class="w-[100%] px-2 bg-transparent outline-none border border-zinc-600 rounded-full" v-model="mensaje" @keydown.enter="mostrarMensaje">
+            <button class="bg-blue-600 p-2 rounded-full"><img :src="'/storage/img/icon-direct.png'"
+                    class="w-[35px]" @click="mostrarMensaje"></button>
+        </div>
+        <div class="fixed top-0 left-0 w-full h-full flex justify-center items-center" :class="{'hidden': !mensajeVisible}">
+            <div class="bg-zinc-800 flex justify-center items-center flex-col p-4 max-w-[450px]">
+                <img :src="'/storage/img/alerta.png'" class="w-[50px] mb-5">
+                <p class="text-center">Recuerda que esta cuenta no te pertenece. Es ilegal enviar mensajes haciendote
+                    pasar por alguién sin su autorización. </p>
+                <p class="text-center mt-3">Las poróximas veces que intentes enviar un mensaje serás penalizado con una
+                    reducción del tiempo</p>
+                <button class="px-2 bg-blue-600 rounded mt-5" @click="ocultarMensaje">Entendido</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import { usePartidaStore } from '@/stores/partidaStore';
+const partidaStore = usePartidaStore();
+const mensajeVisible = ref(false)
+const mensaje = ref('')
+
+const mostrarMensaje = async () => {
+    if (mensaje.value.trim() === '') return;
+
+    try {
+        // Consultamos en la BD si "penalizar" es true o false
+        const response = await partidaStore.comprobarPartida();
+        const penalizar = response.data.penalizar;
+        const idPartida = response.data.id_partida
+
+        if (penalizar) {
+            // Si está penalizado, reducimos el tiempo y mostramos el mensaje de advertencia
+            await partidaStore.reducirTiempo(180); // Penalización de 30 segundos
+            mensaje.value = ''
+        } else {
+            mensajeVisible.value = true;
+            await partidaStore.cambiarEstado('penalizar', idPartida)
+            mensaje.value = ''
+        }
+    } catch (error) {
+        console.error('Error al verificar la penalización:', error);
+    }
+};
+
+const ocultarMensaje = () => {
+    mensajeVisible.value = false
+}
+</script>
+
+<style scoped></style>
