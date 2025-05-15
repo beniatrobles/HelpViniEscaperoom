6b54e23b284f5a1d40a3a067207290f0c0e0dee3
beniatrobles <b.roblesberasaluce@gmail.com>
2025-05-15 16:48:12 +0200

    seeders actuaizados

--- database/seeders/PartidasSeeder.php
+++ database/seeders/PartidasSeeder.php
@@ -9,8 +9,6 @@ class PartidasSeeder extends Seeder
 {
     public function run()
     {
-    
-    
         \App\Models\Partida::create([
             'id_usuario' => 1,
             'tiempo' => 150,
@@ -20,8 +18,11 @@ class PartidasSeeder extends Seeder
             'instagram' => true,
             'twitter' => false,
             'whatsapp' => true,
+            'primera_vez' => true,
+            'terminado' => true,
+            'penalizar' => false,
         ]);
-    
+
         \App\Models\Partida::create([
             'id_usuario' => 1,
             'tiempo' => 250,
@@ -31,6 +32,65 @@ class PartidasSeeder extends Seeder
             'instagram' => false,
             'twitter' => true,
             'whatsapp' => true,
+            'primera_vez' => true,
+            'terminado' => true,
+            'penalizar' => false,
+        ]);
+
+        \App\Models\Partida::create([
+            'id_usuario' => 2,
+            'tiempo' => 170,
+            'completado' => true,
+            'tablet' => true,
+            'gmail' => true,
+            'instagram' => true,
+            'twitter' => true,
+            'whatsapp' => true,
+            'primera_vez' => true,
+            'terminado' => true,
+            'penalizar' => false,
+        ]);
+
+        \App\Models\Partida::create([
+            'id_usuario' => 3,
+            'tiempo' => 220,
+            'completado' => true,
+            'tablet' => true,
+            'gmail' => true,
+            'instagram' => true,
+            'twitter' => true,
+            'whatsapp' => true,
+            'primera_vez' => true,
+            'terminado' => true,
+            'penalizar' => false,
+        ]);
+
+        \App\Models\Partida::create([
+            'id_usuario' => 4,
+            'tiempo' => 175,
+            'completado' => true,
+            'tablet' => true,
+            'gmail' => true,
+            'instagram' => true,
+            'twitter' => true,
+            'whatsapp' => true,
+            'primera_vez' => true,
+            'terminado' => true,
+            'penalizar' => false,
+        ]);
+
+        \App\Models\Partida::create([
+            'id_usuario' => 5,
+            'tiempo' => 230,
+            'completado' => true,
+            'tablet' => true,
+            'gmail' => true,
+            'instagram' => true,
+            'twitter' => true,
+            'whatsapp' => true,
+            'primera_vez' => true,
+            'terminado' => true,
+            'penalizar' => false,
         ]);
     }
 }
--- database/seeders/UsuarioSeeder.php
+++ database/seeders/UsuarioSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-//Usamos la clase Carbon para establecer las fechas created_at y updated_at como la fecha y hora actuales.
 use Carbon\Carbon;
 
 class UsuarioSeeder extends Seeder
@@ -12,13 +11,51 @@ class UsuarioSeeder extends Seeder
     public function run()
     {
         DB::table('usuarios')->insert([
-            'id_usuario' => 1,
-            'nombre_usuario' => 'admin',
-            'correo' => 'admin@admin.com',
-            'contrasena' => bcrypt('1234'),  
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now(),
-            'id_rol' => 3,  // Asignar rol de administrador
+            [
+                'id_usuario' => 1,
+                'nombre_usuario' => 'admin',
+                'correo' => 'admin@admin.com',
+                'contrasena' => bcrypt('1234'),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+                'id_rol' => 3,
+            ],
+            [
+                'id_usuario' => 2,
+                'nombre_usuario' => 'imanol',
+                'correo' => 'imanol@admin.com',
+                'contrasena' => bcrypt('1234'),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+                'id_rol' => 3,
+            ],
+            [
+                'id_usuario' => 3,
+                'nombre_usuario' => 'aitor',
+                'correo' => 'aitor@admin.com',
+                'contrasena' => bcrypt('1234'),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+                'id_rol' => 3,
+            ],
+            [
+                'id_usuario' => 4,
+                'nombre_usuario' => 'anurag',
+                'correo' => 'anurag@admin.com',
+                'contrasena' => bcrypt('1234'),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+                'id_rol' => 3,
+            ],
+            [
+                'id_usuario' => 5,
+                'nombre_usuario' => 'beñat',
+                'correo' => 'benat@admin.com',
+                'contrasena' => bcrypt('1234'),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+                'id_rol' => 3,
+            ],
         ]);
     }
 }
