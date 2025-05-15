3f1ca370af103ae3739f7ef3f89da94f7064cd46
ik_2dw3a <ik_2dw3a@H015110.NET>
2025-05-15 09:04:28 +0200

    publicaciones e historias destacadas hecjas

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -9,7 +9,6 @@
       </video>
       <h1 class="text-[40px] max-w-max overflow-hidden whitespace-nowrap titulo__inicio z-10">Help <span class="text-[#0ED800]">Vini</span> Escape Room</h1>
       <img :src="'/storage/img/Logo.png'" class="img__inicio" width="450px">
-      <p class="parrafo__inicio mb-3">Lanzate a la aventura y ayuda a Vini a recuperar sus redes sociales</p>
       <router-link to="/story" class="bg-[#0ED800] px-[180px] py-[35px] text-xl active:shadow-none active:translate-y-[15px] rounded-[5px] btn__inicio z-10">Comencemos</router-link>
     </div>
   </template>
